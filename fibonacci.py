68cee723313641d928c6b37c0a85d7909f0fed7e
Nigraum <yan.corintima@hotmail.com>
2023-03-02 16:48:22 -0300

    Finishing fibonacci problem

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,11 @@
-print("Hello")
+n = int(input("Informe o número: "))
+
+a, b = 0, 1
+
+while a < n:
+    a, b = b, a + b
+
+if a == n:
+    print("O número pertence a sequência")
+else:
+    print("O número não pertence a sequência")
